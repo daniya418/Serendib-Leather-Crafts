6031a5ec345b61d07a623a84e0349945ffc4d081
KavinduAbeysinghe <118504728+KavinduAbeysinghe@users.noreply.github.com>
2023-04-05 14:10:17 +0530

    Update splash.js

--- splash.js
+++ splash.js
@@ -21,7 +21,7 @@ window.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
       intro.style.top = "-100vh";
       setTimeout(() => {
-        window.location.href = "../main-page/main.html";
+        window.location.href = "./main-page/main.html";
       }, 700);
     }, 5000);
   });
